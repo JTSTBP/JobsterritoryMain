9f853f7df84d73e7f37eab65fe864f30dfe12f11
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2025-09-10 11:16:46 +0530

    mych

--- adminpanel/src/index.js
+++ adminpanel/src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/admin">
+   
       <App />
-    </BrowserRouter>
+
   </React.StrictMode>
 );
 
